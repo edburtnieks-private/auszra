4ada8d8bd93ef94e92f4fe567cc246320765f069
Edgar Burtnieks <edburtnieks@gmail.com>
2020-07-29 11:46:28 +0300

    :sparkles: Change to SSR

--- pages/coming-soon.vue
+++ pages/coming-soon.vue
@@ -64,13 +64,11 @@
           name="newsletter"
           method="POST"
           data-netlify="true"
+          netlify-honeypot="bot-field"
           @submit.prevent="handleSubmit"
         >
           <p class="hidden">
-            <label>
-              Don’t fill this field:
-              <input name="bot-field" :value="botField" readonly />
-            </label>
+            <label>Don’t fill this field: <input name="bot-field" /></label>
           </p>
 
           <label
@@ -225,7 +223,6 @@ export default {
     return {
       formData: {
         email: '',
-        botField: '',
       },
       isLoading: false,
       isDone: false,
@@ -302,7 +299,7 @@ export default {
         if (!isInputValid) errors[key] = message
       }
 
-      if (Object.keys(errors).length > 0 || this.bodField) isValid = false
+      if (Object.keys(errors).length > 0) isValid = false
 
       return {
         errors,
